d7ae938e04e05e8040dec0472856d97a54ee531f
yangbodong22011 <491219935@qq.com>
2017-11-24 18:12:38 +0800

    del

--- example/hello/.hellomod.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yangbodong/kernel/hello/hellomod.o := ld -Map /var/tmp/hello.txt   -r -o /home/yangbodong/kernel/hello/hellomod.o /home/yangbodong/kernel/hello/hello.o ; scripts/mod/modpost /home/yangbodong/kernel/hello/hellomod.o
