8c080e02d1d1800b98fe4b3e6294290ca127dd3b
yangbodong22011 <491219935@qq.com>
2017-12-12 22:00:49 +0800

    module

--- module.sh
+++ module.sh
@@ -2,6 +2,7 @@
 
 pwd=$(cd `dirname $0`; pwd)
 dirname=$1
+filename=`echo $dirname | awk -F "/" '{print $NF}'`
 mkdir $pwd/$dirname
 cp $pwd/Makefile $pwd/$dirname
-cp $pwd/module.c $pwd/$dirname/$dirname.c
+cp $pwd/module.c $pwd/$dirname/$filename.c
